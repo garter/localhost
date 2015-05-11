c7ecfee86a429ffda9d861405c7cdab6a2769075
garter <bayram88@live.ru>
2015-05-11 07:41:56 +0300

    создали класс user c тремя параметрами name email country

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php
-phpinfo();
+class User{
+    public $name;
+    public $email;
+    public $country;
+
+    public function  __construct($name, $email, $country){
+        $this->name = $name;
+        $this->email = $email;
+        $this->country = $country;
+    }
+
+
+}
+
+$bayram = new User("Bayram", "bayram88@live.ru", "Tadjikistan");
+echo $bayram->email;
+$bayram->email = "bairam.hamraev@mail.ru";
+echo "<br>";
+echo $bayram->email;
