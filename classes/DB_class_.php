3378f65a4a2600555b20b1b3d5845370ab7efdac
garter <bayram88@live.ru>
2015-05-11 09:10:31 +0300

    Содание нового класса DB

--- classes/DB_class_.php
+++ classes/DB_class_.php
@@ -7,5 +7,20 @@
  */
 
 class DB {
+    const DB_HOST = "localhost";
+    const DB_USER = "root";
+    const DB_PASS = "";
+    const DB_NAME = "bayram";
 
-} 
+    public function __constuct(){
+        mysqli_connect(DB::DB_HOST, DB::DB_USER, DB::DB_PASS, DB::DB_NAME);
+
+    }
+
+}
+
+$db = new DB();
+$res = $db->query("SELECT * FROM `BORYA`"):
+$row = fetch_assoc($res);
+
+echo var_dump($row);
