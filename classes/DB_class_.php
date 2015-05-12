b89d6655550411655602dcbc894564c0ebdcc006
garter <bayram88@live.ru>
2015-05-12 07:08:25 +0300

    мастер класс от препода

--- classes/DB_class_.php
+++ classes/DB_class_.php
@@ -7,20 +7,13 @@
  */
 
 class DB {
-    const DB_HOST = "localhost";
-    const DB_USER = "root";
-    const DB_PASS = "";
-    const DB_NAME = "bayram";
 
-    public function __constuct(){
-        mysqli_connect(DB::DB_HOST, DB::DB_USER, DB::DB_PASS, DB::DB_NAME);
+    public function __constuct()
+    {
+        mysql_connect('localhost', 'root', '');
+        mysql_select_db('test');
 
     }
 
 }
 
-$db = new DB();
-$res = $db->query("SELECT * FROM `BORYA`"):
-$row = fetch_assoc($res);
-
-echo var_dump($row);
--- index.php
+++ index.php
@@ -1,20 +1,4 @@
 <?php
-class User{
-    public $name;
-    public $email;
-    public $country;
+require_once __DIR__."classes/DB_class_.php";
 
-    public function  __construct($name, $email, $country){
-        $this->name = $name;
-        $this->email = $email;
-        $this->country = $country;
-    }
-
-
-}
-
-$bayram = new User("Bayram", "bayram88@live.ru", "Tadjikistan");
-echo $bayram->email;
-$bayram->email = "bairam.hamraev@mail.ru";
-echo "<br>";
-echo $bayram->email;
+$db = new DB();
